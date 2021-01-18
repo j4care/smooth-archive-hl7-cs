4bc48b8b0c5b9ac9b5fb2bac98d29240da18d5f5
petr <peta.kalina@gmai.com>
2021-01-18 11:37:27 +0100

    5.23.0.RC5

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,8 @@ import shlex
 project = u'Smooth Archive - HL7 Conformance Statement'
 copyright = u'2020, J4Care GmBH'
 author = u'J4Care GMbH'
-version = u'5.22.6'
-release = u'5.22.6.GA'
+version = u'5.23.0'
+release = u'5.23.0.RC5'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
