4b32117b59d3bc19aa776bf54669c01b4463bc2b
petr <peta.kalina@gmai.com>
2022-04-25 14:16:25 +0200

    https://redmine.j4care.com:8443/issues/15167

--- docs/conf.py
+++ docs/conf.py
@@ -21,8 +21,8 @@ import shlex
 project = u'Smooth Archive - HL7 Conformance Statement'
 copyright = u'2021, J4Care GmBH'
 author = u'J4Care GMbH'
-version = u'5.25'
-release = u'5.25'
+version = u'5.26'
+release = u'5.26'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
