50cea4a1e5cdd2d83d208895fbdb333c222dae1c
petr <peta.kalina@gmai.com>
2022-11-28 17:22:39 +0100

    release 5.29.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ project = u'Smooth Archive - HL7 Conformance Statement'
 copyright = u'2022, J4Care GmBH'
 author = u'J4Care GMbH'
 version = u'5.29'
-release = u'5.29'
+release = u'5.29.1'
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
